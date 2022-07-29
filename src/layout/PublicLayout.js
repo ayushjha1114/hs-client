5ffc6bdacb584235dec7df38f5a96a2ce1549698
ayush.jha <ayush.jha@tothenew.com>
2022-07-30 00:39:40 +0530

    Feature: Added footer in UI

--- src/layout/PublicLayout.js
+++ src/layout/PublicLayout.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Footer from '../modules/auth/Footer';
 
 let PublicPage = (props) => {
 
   return (
+    <>
       <div className="homeContainer">
           <img src="assets/images/home.png" alt="" style={{ maxWidth:'100%', height: 'auto', marginTop: '100px'}} />
         {/* <div className='HomeNavBar'> */}
           <div className="HomeNavBarImg">
-            <img src="assets/images/DGsoft-logo.svg" alt="" style={{ maxWidth: '25%' }}/>
+            <img src="assets/images/DGSoft-logo.png" alt="" style={{ maxWidth: '25%' }}/>
           </div>
           <div className="HomeNavBarItem">
             <Link to="/" className="navLink"> Home</Link>
             <Link to="/products" className="navLink"> Product</Link>
             <Link to="/services" className="navLink"> Service</Link>
             <Link to="/amc-plans" className="navLink"> AMC</Link>
-            <Link to="/" className="navLink"> Tech News</Link>
+            {/* <Link to="/" className="navLink"> Tech News</Link> */}
             <Link to="/about-us" className="navLink"> About Us</Link>
             <Link to="/contact-us" className="navLink"> Contact Us</Link>
             <Link to="/login" className="navLoginBtn"> Login</Link>
@@ -23,6 +25,8 @@ let PublicPage = (props) => {
         {/* </div> */}
         {props.children}
       </div>
+      <Footer />
+    </>
   )
 }
 
