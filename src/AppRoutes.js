141871508afeefb95744769ddf3065d0f80a23af
singh-jitendra <jitendra.singh31@outlook.com>
2022-08-18 22:42:48 +0530

    Ui for service and amc plan complete

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import {
-    Routes,
-    Route,
-} from 'react-router';
+import React from "react";
+import { Routes, Route } from "react-router";
 
-import { notLoggedIn, authenticatedUsersOnly } from './util/middleware/index';
-import WelcomePage from './modules/auth/Welcome';
-import LoginPage from './modules/auth/Login';
-import AMCPlans from './modules/auth/AMCPlans';
-import AMCPlanDetails from './modules/auth/AMCPlanDetails';
-import Dashboard from './modules/dashboard/Dashboard';
-import Services from './modules/auth/Services'
-import PublicPage from './layout/PublicLayout';
+import { notLoggedIn, authenticatedUsersOnly } from "./util/middleware/index";
+import WelcomePage from "./modules/auth/Welcome";
+import LoginPage from "./modules/auth/Login";
+import AMCPlanDetails from "./modules/auth/AMCPlanDetails";
+import Dashboard from "./modules/dashboard/Dashboard";
+import Services from "./modules/auth/Services";
+import PublicPage from "./layout/PublicLayout";
 
 // import ResetPasswordPage from './modules/auth/ResetPassword';
 // import ValidateTokenPage from './modules/auth/ValidateToken';
@@ -21,18 +17,16 @@ import PublicPage from './layout/PublicLayout';
 // httpService.setupInterceptors();
 
 const AppRoutes = (props) => {
-    return (
-        <Routes>
-            <Route path="/" element={<PublicPage />} />
-            <Route path="/amc-plans" element={<AMCPlans />} />
-            <Route path="/amc-plans-details" element={<AMCPlanDetails />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/services" element={<Services />} />
-            <Route path='/welcome' element={<WelcomePage />}></Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<PublicPage />} />
+      <Route path="/amc-plans-details" element={<AMCPlanDetails />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/welcome" element={<WelcomePage />}></Route>
+    </Routes>
+  );
 };
-
 
 export default AppRoutes;
